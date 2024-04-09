390875c55de59da7fc18a08a8c1b60c8ed442586
Francisco VIllarreal <paco_vip95@hotmail.com>
2024-04-08 20:31:18 -0600

    update

---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import ToggleTheme from "./toggleTheme";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="p-[10px] pl-5 flex justify-between items-center h-20 max-w-5xl mx-auto">
+        <div>
+          <Link href="/" className="flex items-center">
+              <Image
+                src="/bear.png"
+                alt={"logo"}
+                width={40}
+                height={40}
+              />
+              <span className="pl-[10px]">Bearpoint</span>
+          </Link>
+        </div>
+        <div>
+          <ToggleTheme/>
+        </div>
+      </div>
+    </header>
+  );
+}
