9d553a89114e85c48aa2403fbb24b96400734e61
Francisco VIllarreal <paco_vip95@hotmail.com>
2024-04-08 21:47:10 -0600

    update types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
-import { ArticleDev } from "@/types/devArticle";
+import { ArticleDev, FetchArticleResponse } from "@/types/devArticle";
+import { ErrorMessage } from "@/types/errorFetch";
 import Link from "next/link";
 
 export default async function Home() {
   
-  const data: ArticleDev[] = await fetch("https://dev.to/api/articles/me", {
+  const data: FetchArticleResponse<ArticleDev, ErrorMessage> = await fetch("https://dev.to/api/articles/me", {
     headers: { "api-key": process.env.DEV_TO_API_KEY || "" },
   }).then((res) => res.json());
 
+  console.log(typeof data, data);
+
+  if(data.status) {
+    return (
+      <div>Error Pulling Articules</div>
+    )
+  }
   return (
     <main className="flex flex-col justify-between max-w-[768px] px-20 mx-auto h-screen">
       <div>
-        {data?.map(({ url, title, description, id, tag_list }) => (
+        <div>
+          ultimos Atriculos 
+        </div>
+        {data?.map(({ url, title, description, id, tag_list }: ArticleDev) => (
           <div key={id} className="my-4">
             <Link href={url} target="_blank" className="text-2xl truncate">
               {title}
--- src/types/devArticle.d.ts
+++ src/types/devArticle.d.ts
@@ -1,33 +1,38 @@
+import { ErrorMessage } from '@/types/errorFetch';
+import { ErrorMessage } from "./errorFetch.d";
+
+export type FetchArticleResponse<T, ErrorMessage> = T[] | ErrorMessage;
+
 export interface ArticleDev {
-    type_of:                  string;
-    id:                       number;
-    title:                    string;
-    description:              string;
-    published:                boolean;
-    published_at:             Date;
-    slug:                     string;
-    path:                     string;
-    url:                      string;
-    comments_count:           number;
-    public_reactions_count:   number;
-    page_views_count:         number;
-    published_timestamp:      Date;
-    body_markdown:            string;
-    positive_reactions_count: number;
-    cover_image:              null;
-    tag_list:                 string[];
-    canonical_url:            string;
-    reading_time_minutes:     number;
-    user:                     User;
+  type_of: string;
+  id: number;
+  title: string;
+  description: string;
+  published: boolean;
+  published_at: Date;
+  slug: string;
+  path: string;
+  url: string;
+  comments_count: number;
+  public_reactions_count: number;
+  page_views_count: number;
+  published_timestamp: Date;
+  body_markdown: string;
+  positive_reactions_count: number;
+  cover_image: null;
+  tag_list: string[];
+  canonical_url: string;
+  reading_time_minutes: number;
+  user: User;
 }
 
 export interface UserDev {
-    name:             string;
-    username:         string;
-    twitter_username: null;
-    github_username:  string;
-    user_id:          number;
-    website_url:      null;
-    profile_image:    string;
-    profile_image_90: string;
+  name: string;
+  username: string;
+  twitter_username: null;
+  github_username: string;
+  user_id: number;
+  website_url: null;
+  profile_image: string;
+  profile_image_90: string;
 }
